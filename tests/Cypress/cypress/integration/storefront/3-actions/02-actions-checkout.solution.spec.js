acae045c19b7573b24dcf96627701027ac1491cd
boxblinkracer <boxblinkracer@gmx.net>
2022-01-11 22:00:19 +0100

    solution for actions

--- tests/Cypress/cypress/integration/storefront/3-actions/02-actions-checkout.solution.spec.js
+++ tests/Cypress/cypress/integration/storefront/3-actions/02-actions-checkout.solution.spec.js
@@ -1,69 +1,35 @@
 import LoginAction from "../../../support/actions/storefront/account/LoginAction";
+import RegisterAction from "../../../support/actions/storefront/account/RegisterAction";
+import Session from "../../../support/actions/utils/Session";
 
-const loginAction = new LoginAction();
+const session = new Session();
 
-
-before(function () {
-    cy.viewport(1920, 1080);
-})
+const register = new RegisterAction();
+const login = new LoginAction();
+// these are just placeholders to demonstrate the approach
+const menu = null;
+const listing = null;
+const pdp = null;
+const checkout = null;
 
 
 it('Full Checkout: Paid in advance', () => {
 
-    // -----------------------------------------------------------------------------------
-    // REGISTER
-    // TODO: extract this
     cy.visit('/account');
+    register.doRegister("cypress@germany.de", "DE123DE123");
 
-    cy.get('#personalSalutation').select('Mr.');
-    cy.get('#personalFirstName').clear().type('Christian');
-    cy.get('#personalLastName').clear().type('Dangl');
-    cy.get('#personalMail').clear().type("cypress@germany.de");
-    cy.get('#personalPassword').clear().type("DE123DE123");
-    cy.get('#billingAddressAddressStreet').clear().type('Cypress');
-    cy.get('#billingAddressAddressZipcode').clear().type('DE');
-    cy.get('#billingAddressAddressCity').clear().type('Germany Town');
-    cy.get('#billingAddressAddressCountry').select('Germany');
-    cy.get('.register-submit > .btn').click();
-
-    // -----------------------------------------------------------------------------------
-    // CLEAR SESSION
-
-    cy.clearCookies();
-    cy.visit('/', {
-        onBeforeLoad: (win) => {
-            win.sessionStorage.clear()
-        }
-    });
-
-    // -----------------------------------------------------------------------------------
-    // LOGIN
+    session.resetSession();
 
     cy.visit('/account');
+    login.doLogin('cypress@germany.de', 'DE123DE123');
 
-    loginAction.doLogin('cypress@germany.de', 'DE123DE123');
+    menu.openFirstCategory();
+    listing.clickOnFirstProduct();
+    pdp.addToCart();
 
-
-    // -----------------------------------------------------------------------------------
-    // PRODUCT
-
-    cy.get('.home-link > .main-navigation-link-text').click();
-    cy.get(':nth-child(1) > .card > .card-body > .product-info > .product-name').click();
-
-    cy.get('.col-4 > .custom-select').select("2");
-    cy.get('.buy-widget-container > .col-8 > .btn').click();
-
-
-    // -----------------------------------------------------------------------------------
-    // CHECKOUT
-
-    cy.get('.begin-checkout-btn').click();
-
-    cy.contains('Paid in advance').click({force: true});
-
-    cy.get('.checkout-confirm-tos-label').click('left');
-
-    cy.get('#confirmFormSubmit').click();
+    checkout.openCart();
+    checkout.switchPaymentMethod('Paid in advance');
+    checkout.placeOrder();
 
     // -----------------------------------------------------------------------------------
 
