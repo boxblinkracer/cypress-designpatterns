03a4edf2101e53d10d7dae962bd3c8b79e9ed12c
boxblinkracer <boxblinkracer@gmx.net>
2022-01-13 00:19:42 +0100

    fix host versions

--- tests/Cypress/cypress/support/actions/utils/Session.js
+++ tests/Cypress/cypress/support/actions/utils/Session.js
@@ -17,15 +17,5 @@ export default class Session {
         });
     }
 
-    /**
-     * Resets only the cookies and storage.
-     * This can be used in test runs to have a lost session.
-     */
-    resetSessionData() {
-        cy.clearLocalStorage();
-        // we have to clear cookies 2x to really make it work
-        cy.clearCookies();
-        cy.clearCookies();
-    }
 
 }
