6cf6525c0192339c9e57d885de9b273d8596fd33
boxblinkracer <boxblinkracer@gmx.net>
2022-01-12 23:49:13 +0100

    add missing steps in test

--- tests/Cypress/cypress/integration/storefront/4-scenarios/01-scenario-checkout.spec.js
+++ tests/Cypress/cypress/integration/storefront/4-scenarios/01-scenario-checkout.spec.js
@@ -1,12 +1,44 @@
 
 it('Full Checkout: Paid in advance', () => {
 
-    // TODO: add our scenario here
+    // -----------------------------------------------------------------------------------
+    // REGISTER
+
+    cy.visit('/account');
+
+    cy.get('#personalSalutation').select('Mr.');
+    cy.get('#personalFirstName').clear().type('Christian');
+    cy.get('#personalLastName').clear().type('Dangl');
+    cy.get('#personalMail').clear().type("cypress@germany.de");
+    cy.get('#personalPassword').clear().type("DE123DE123");
+    cy.get('#billingAddressAddressStreet').clear().type('Cypress');
+    cy.get('#billingAddressAddressZipcode').clear().type('DE');
+    cy.get('#billingAddressAddressCity').clear().type('Germany Town');
+    cy.get('#billingAddressAddressCountry').select('Germany');
+    cy.get('.register-submit > .btn').click();
+
+    // -----------------------------------------------------------------------------------
+    // CLEAR SESSION
+
+    cy.clearCookies();
+    cy.visit('/', {
+        onBeforeLoad: (win) => {
+            win.sessionStorage.clear()
+        }
+    });
+
+    // -----------------------------------------------------------------------------------
+    // LOGIN
+
+    cy.visit('/account');
+
+    loginPage.doLogin('cypress@germany.de', 'DE123DE123');
+    cy.pause();
 
     // -----------------------------------------------------------------------------------
     // PRODUCT
 
-    cy.get('.home-link > .main-navigation-link-text').click();
+    cy.get('[href="http://localhost/Clothing/"] > .main-navigation-link-text > span').click();
     cy.get(':nth-child(1) > .card > .card-body > .product-info > .product-name').click();
 
     cy.get('.col-4 > .custom-select').select("2");
