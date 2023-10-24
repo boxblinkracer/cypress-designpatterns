eac8d6d10b756d2bb2727b8a658874e039c32805
Christian Dangl <cd@dasistweb.de>
2023-10-24 11:10:27 +0200

    remove sample test

--- tests/Cypress/cypress/e2e/storefront/1-basics/01-start.cy.js
+++ tests/Cypress/cypress/e2e/storefront/1-basics/01-start.cy.js
@@ -1,10 +1,5 @@
-it.only('Sample Test', () => {
+it('Sample Test', () => {
 
     cy.visit('/');
-
-    cy.get('#accountWidget > .icon > svg').click();
-    cy.get('.account-menu-login > .btn').click();
-
-    cy.get('#personalFirstName').clear().type('Christian');
 
 })
